9d0ad0af5d4044f8c20cfcf04308ee15fc9d40aa
unnikrishnan803 <unnikrishnan803@users.noreply.github.com>
2025-09-02 07:18:17 +0530

    Update Vercel configuration to fix deployment issues

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Make migrations
+python manage.py makemigrations
+python manage.py migrate
