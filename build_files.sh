c777cf807530e2c4470942b4c21009efbc8bfb1e
unnikrishnan803 <unnikrishnan803@users.noreply.github.com>
2025-09-02 07:21:10 +0530

    Fix Vercel build configuration and Python setup

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# Create and activate virtual environment
+python3.9 -m venv venv
+source venv/bin/activate
+
 # Install dependencies
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 # Collect static files
